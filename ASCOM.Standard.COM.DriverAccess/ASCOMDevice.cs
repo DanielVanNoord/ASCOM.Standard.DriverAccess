76d8cbcd7aade1f4d53683413ce83db26abfa3d0
Daniel Van Noord <sg1mash98@gmail.com>
2020-12-22 21:37:05 -0500

    Added basic ASCOM Device access

---
+++ ASCOM.Standard.COM.DriverAccess/ASCOMDevice.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASCOM.Standard.COM.DriverAccess
+{
+    public class ASCOMDevice : Interfaces.IAscomDevice
+    {
+        private dynamic device;
+        internal dynamic Device
+        {
+            get => device;
+        }
+
+        private short? interfaceVersion = null;
+
+        public ASCOMDevice(string progid)
+        {
+            device = new DynamicAccess(progid);
+        }
+
+        public bool Connected { get => Device.Connected; set => Device.Connected = value; }
+
+        public string Description => Device.Description;
+
+        public string DriverInfo => Device.DriverInfo;
+
+        public string DriverVersion => Device.DriverVersion;
+
+        public short InterfaceVersion
+        {
+            get
+            {
+                // Test whether the interface version has already been retrieved
+                if (!interfaceVersion.HasValue) // This is the first time the method has been called so get the interface version number from the driver and cache it
+                {
+                    try { interfaceVersion = Device.InterfaceVersion; } // Get the interface version
+                    catch { interfaceVersion = 1; } // The method failed so assume that the driver has a version 1 interface where the InterfaceVersion method is not implemented
+                }
+
+                return interfaceVersion.Value; // Return the newly retrieved or already cached value
+            }
+        }           
+
+        public string Name => Device.Name;
+
+        public IList<string> SupportedActions => (Device.SupportedActions as ArrayList).Cast<string>().ToList();
+
+        public string Action(string ActionName, string ActionParameters)
+        {
+            return Device.Action(ActionName, ActionParameters);
+        }
+
+        public void CommandBlind(string Command, bool Raw = false)
+        {
+            Device.CommandBlind(Command, Raw);
+        }
+
+        public bool CommandBool(string Command, bool Raw = false)
+        {
+            return Device.CommandBool(Command, Raw);
+        }
+
+        public string CommandString(string Command, bool Raw = false)
+        {
+            return Device.CommandString(Command, Raw);
+        }
+
+        public void Dispose()
+        {
+            Device.Dispose();
+        }
+
+        public void SetupDialog()
+        {
+            Device.SetupDialog();
+        }
+
+        internal void AssertMethodImplemented(int MinimumVersion, string Message)
+        {
+            if(this.InterfaceVersion < MinimumVersion)
+            {
+                throw new ASCOM.MethodNotImplementedException(Message);
+            }
+        }
+
+        internal void AssertPropertyImplemented(int MinimumVersion, string Message)
+        {
+            if (this.InterfaceVersion < MinimumVersion)
+            {
+                throw new ASCOM.PropertyNotImplementedException(Message);
+            }
+        }
+    }
+}
---
+++ ASCOM.Standard.COM.DriverAccess/DynamicAccess.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Dynamic;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace ASCOM.Standard.COM.DriverAccess
+{
+    public class DynamicAccess : DynamicObject
+    {
+        private object device;
+
+        internal object Device
+        {
+            get => device;
+        }
+
+        public DynamicAccess(string ProgID)
+        {
+            Type type = Type.GetTypeFromProgID(ProgID);
+            device = Activator.CreateInstance(type);
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            try
+            {
+                result = Unwrap().GetType().InvokeMember(
+             binder.Name,
+             BindingFlags.InvokeMethod,
+             Type.DefaultBinder,
+             Unwrap(),
+             args
+         );
+
+                return true;
+            }
+            catch (TargetInvocationException ex)
+            {
+                if (ex.InnerException?.HResult == ErrorCodes.NotImplemented)
+                {
+                    var member = (string)ex.InnerException.GetType().InvokeMember("PropertyOrMethod", BindingFlags.Default | BindingFlags.GetProperty, null, ex.InnerException, new object[] { }, CultureInfo.InvariantCulture);
+                    //TL.LogMessageCrLf(binder.Name, "  Throwing MethodNotImplementedException: '" + member + "'");
+                    throw new MethodNotImplementedException(member, ex.InnerException);
+                }
+
+                CheckDotNetExceptions(binder.Name, ex);
+
+                throw;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            try
+            {
+                Unwrap().GetType().InvokeMember(
+            binder.Name,
+            BindingFlags.SetProperty,
+            Type.DefaultBinder,
+            Unwrap(),
+            new object[] { value }
+        );
+                return true;
+            }
+            catch (TargetInvocationException ex)
+            {
+                CheckDotNetExceptions(binder.Name, ex);
+
+                if (ex.InnerException?.HResult == ErrorCodes.NotImplemented)
+                {
+                    var member = (string)ex.InnerException.GetType().InvokeMember("PropertyOrMethod", BindingFlags.Default | BindingFlags.GetProperty, null, ex.InnerException, new object[] { }, CultureInfo.InvariantCulture);
+                    //TL.LogMessageCrLf(binder.Name, "  Throwing PropertyNotImplementedException: '" + member + "'");
+                    throw new PropertyNotImplementedException(member, false, ex.InnerException);
+                }
+
+                throw;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            try
+            {
+                result = Unwrap().GetType().InvokeMember(
+                binder.Name,
+                BindingFlags.GetProperty,
+                Type.DefaultBinder,
+                Unwrap(),
+                new object[] { },
+                CultureInfo.InvariantCulture
+            );
+
+                return true;
+            }
+            catch (TargetInvocationException ex)
+            {
+                if (ex.InnerException?.HResult == ErrorCodes.NotImplemented)
+                {
+                    var member = (string)ex.InnerException.GetType().InvokeMember("PropertyOrMethod", BindingFlags.Default | BindingFlags.GetProperty, null, ex.InnerException, new object[] { }, CultureInfo.InvariantCulture);
+                    //TL.LogMessageCrLf(binder.Name, "  Throwing PropertyNotImplementedException: '" + member + "'");
+                    throw new PropertyNotImplementedException(member, false, ex.InnerException);
+                }
+
+                CheckDotNetExceptions(binder.Name, ex);
+
+                throw;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private object Unwrap()
+       => Device ?? throw new ObjectDisposedException(nameof(Device));
+
+        /// <summary>
+        /// Checks for ASCOM exceptions returned as inner exceptions of TargetInvocationException. When new ASCOM exceptions are created
+        /// they must be added to this method. They will then be used in all three cases of Property Get, Property Set and Method call.
+        /// </summary>
+        /// <param name="memberName">The name of the invoked member</param>
+        /// <param name="e">The thrown TargetInvocationException including the inner exception</param>
+        private void CheckDotNetExceptions(string memberName, Exception e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
