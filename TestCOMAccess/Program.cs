1ee0e2fa96e308d5f0291ddd4d569a16d47d3f43
Daniel Van Noord <sg1mash98@gmail.com>
2020-12-22 21:44:36 -0500

    Added simple test program

---
+++ TestCOMAccess/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TestCOMAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var focuser = new ASCOM.Standard.COM.DriverAccess.Focuser("ASCOM.Simulator.Focuser");
+
+            try
+            {
+                focuser.Connected = true;
+                Console.WriteLine(focuser.Connected);
+                focuser.Halt();
+                Console.WriteLine(focuser.StepSize);
+            }
+            catch(ASCOM.PropertyNotImplementedException ex)
+            {
+
+            }
+            catch (ASCOM.NotConnectedException)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                var HResult = (UInt32)ex.HResult;
+
+                var type = ex.GetType();
+
+            }
+
+            focuser.SetupDialog();
+
+            Console.WriteLine(focuser.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
